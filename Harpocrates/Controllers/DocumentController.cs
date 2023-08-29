7c2d7fb909b5b0069de17858e8d4742c0c7a6c78
Robbe Louwet <robbe.louwet@nara.to>
2023-08-29 13:29:27 +0200

    init

---
+++ Harpocrates/Controllers/DocumentController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Harpocrates.API.Controllers;
+
+public class DocumentController : Controller
+{
+    public DocumentController()
+    {
+        
+    }
+}
--- Harpocrates/Program.cs
+++ Harpocrates/Program.cs
@@ -9,14 +9,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<CosmosClient>();
+// builder.Services.AddSingleton<CosmosClient>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();  
     app.UseSwaggerUI();
 }
 
